69d5c80bb13c60662c58bd5676b998d691c046f9
kevinhorsch@hotmail.com <root@KevinsAlien>
2023-02-18 19:26:23 -0600

    removed comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -1,15 +1,8 @@
-/**
-* Comment
-* This is a calculator
-*/
 package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-/**
-* comment this is a calculator
-* it really is just for learning
-*/
+
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
